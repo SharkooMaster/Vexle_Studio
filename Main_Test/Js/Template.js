e5337accb1f87252bbbfc8f373b0bcbba50e8f61
Sharkoo <alirashini@hotmail.com>
2019-07-18 00:11:06 +0200

    Site version 0.9
    responsitivity problem on phones. other whise ok

--- Main_Test/Js/Template.js
+++ Main_Test/Js/Template.js
@@ -4,11 +4,9 @@ var Drop = false;
 function NavBar_Toggle(){
     if(!Drop){
         document.getElementById("NavBar_Phone_Drop").style.display = "block";
-        document.getElementById("Nav_Bar_Temp").style.height = "150px";
         Drop = true;
     }else if(Drop){
         document.getElementById("NavBar_Phone_Drop").style.display = "none";
-        document.getElementById("Nav_Bar_Temp").style.height = "50px";
         Drop = false;
     }
 }
